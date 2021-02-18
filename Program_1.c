fa2858794a9aaea5a89fff372709abc977cfc154
Rifah A Khan <rifahakhan@gmail.com>
2021-02-18 13:51:14 +0000

    Program to accept and display the array elements.

--- Program_1.c
+++ Program_1.c
@@ -15,6 +15,18 @@ int main()
    //Read the Value of N using scanf statement
    //Read the array elements using for loop and scanf statment
    //Print the array elements in using printf statement
-   
-   return 0; 
+  int N,a[100],i;
+  printf("Enter the size of array\n");
+  scanf("%d",&N);
+  printf("Enter the array elements\n");
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("The array elements are\n");
+  for(i=0;i<N;i++)
+  {
+    printf("%d\t",a[i]);
+  }
+     return 0; 
 }
