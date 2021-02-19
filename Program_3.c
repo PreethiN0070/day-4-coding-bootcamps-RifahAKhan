d3c1170f0d45cc7837281a4f75bf90c2d7021b59
Rifah A Khan <rifahakhan@gmail.com>
2021-02-19 14:00:43 +0000

    Program to get two rows and columns of integers from the user and print them in matrix fashion.

--- Program_3.c
+++ Program_3.c
@@ -13,5 +13,28 @@ int main()
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
    //Read the elements of matrix a using nested for loop and scanf statment
    //Print the elements of matrix a using nested for loop and printf statment as per the above specified format
+   int i,j,m,n;
+   int matrix[10][20];
+   printf("Enter number of rows: ");
+   scanf("%d",&m);
+   printf("Enter number of columns ");
+   scanf("%d",&n);
+
+   for(i=0;i<m;i++)
+   {
+     for(j=0;j<n;j++)
+     {
+       printf("Enter data in [%d][%d]: ",i,j);
+       scanf("%d",&matrix[i][j]);
+     }
+   }
+
+   for(i=0;i<m;i++){
+     for(j=0;j<n;j++){
+       printf("%d\t",matrix[i][j]);
+     }
+     printf("\n");
+   }
+  
   return 0;
 }
